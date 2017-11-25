214f3ad12b77da7df5ae0387e579653e9ad93bf7
David <david_thedragonzzz@yahoo.com>
2017-11-25 13:45:07 +0700

    buat setting database

---
+++ app/src/main/java/com/mobile/pos/SettingActivity.java
@@ -0,0 +1,50 @@
+package com.mobile.pos;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SettingActivity extends AppCompatActivity {
+    EditText teksIP, teksUser, teksPass;
+    SharedPreferences getPrefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        teksIP = (EditText) findViewById(R.id.ip);
+        teksUser = (EditText) findViewById(R.id.username);
+        teksPass = (EditText) findViewById(R.id.password);
+        getPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        Button button = (Button) findViewById(R.id.ok);
+        Button kembali = (Button) findViewById(R.id.kembali);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Constant.ip = teksIP.getText().toString();
+                Constant.username = teksUser.getText().toString();
+                Constant.password = teksPass.getText().toString();
+                SharedPreferences.Editor e = getPrefs.edit();
+                e.putString("ip", Constant.ip);
+                e.putString("username", Constant.username);
+                e.putString("password", Constant.password);
+                e.apply();
+                Toast.makeText(SettingActivity.this, "Setting berhasil diubah", Toast.LENGTH_SHORT).show();
+            }
+        });
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SettingActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
